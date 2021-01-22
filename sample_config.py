188235bc69fcd3d0edfc708cb181a9c3e960b3cf
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-01-22 10:01:11 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
-bot_token = "1542143453:AAEiNiGq2SgIIsFTNNtcPmPHFSBEwlOGFlM"
+bot_token = "1542143453:AAFLHjqD28FyU2uzrfZaQa6P8MDuQs4dvvc"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
 sudo_chat_id = -1001242690550
 owner_id = 1078841825
